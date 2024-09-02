3e3ca3c21f230d66a1e6a9c7341744282f13157c
renatorv <renatorv@gmail.com>
2024-09-01 21:13:41 -0300

    Listando as visitas na tela inicial

--- example/lib/routes/navigation.dart
+++
@@ -1,4 +0,0 @@
-export '../screens/navigation/breadcrumb_bar.dart';
-export '../screens/navigation/navigation_view.dart';
-export '../screens/navigation/tab_view.dart';
-export '../screens/navigation/tree_view.dart';
